9ae83228d5448538c694309c79071b3ba01543ae
TimofeyKuznetsov <124498386+TimofeyKuznetsov@users.noreply.github.com>
2023-02-05 21:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ def Perimeter(a,b,c,d):
 def Perimeter_multideck(a,b,c,d):
     for i in range(b):
         for j in range(d):
-            game_board[_players_digital_number][a+i][c+j]=2
+            game_board[players_digital_number][a+i][c+j]=2
 def Exception_e_brief(a):
     if ord(a)==1050:
         return 1
     else:
         return 0
 def Letter_number_of_the_player():
-    if the_players_digital_number==0:
+    if players_digital_number==0:
         return 'перв'
     else:
         return 'второ'
-for the_players_digital_number in range(2):
+for players_digital_number in range(2):
     letter_number_of_the_player=Letter_number_of_the_player()[0].upper()+Letter_number_of_the_player()[1:]+'ый'
     print(letter_number_of_the_player+' игрок расставляет корабли.')
     for digital_number_of_the_ship in range(1,5):
@@ -43,7 +43,7 @@ for the_players_digital_number in range(2):
             Y=Exception_e_brief(array_with_variables[0])
             the_first_cordinate=ord(array_with_variables[0])-1040-Y
             the_second_cordinate=int(array_with_variables[1])-1
-            if game_board[the_players_digital_number][the_first_cordinate][the_second_cordinate]!=1 and game_board[the_players_digital_number][the_first_cordinate][the_second_cordinate]!=2:
+            if game_board[players_digital_number][the_first_cordinate][the_second_cordinate]!=1 and game_board[players_digital_number][the_first_cordinate][the_second_cordinate]!=2:
                 break
 #        for i in range(3):
 #           for j in range(3):
@@ -103,23 +103,23 @@ for the_players_digital_number in range(2):
                 elif p==1:
                     g='Данные кординаты недопустимы. Введите к'
                 array_with_variables=input(g+'ординаты '+letter_number_of_the_ship+ur+'палубного корабля через пробел: ').split()
-                Y0=Exception_e_brief(Array_with_variables[0])
-                Y1=Exception_e_brief(Array_with_variables[2])
-                the_first_initial_cordinate=ord(Array_with_variables[0])-1040-Y0
-                the_second_initial_cordinate=int(Array_with_variables[1])-1
-                the_first_final_cordinate=ord(Array_with_variables[2])-1040-Y1
-                the_second_final_cordinate=int(Array_with_variables[3])-1
+                Y0=Exception_e_brief(array_with_variables[0])
+                Y1=Exception_e_brief(array_with_variables[2])
+                the_first_initial_cordinate=ord(array_with_variables[0])-1040-Y0
+                the_second_initial_cordinate=int(array_with_variables[1])-1
+                the_first_final_cordinate=ord(array_with_variables[2])-1040-Y1
+                the_second_final_cordinate=int(array_with_variables[3])-1
                 if the_first_initial_cordinate > the_first_final_cordinate:
-                    the_first_initial_cordinate,the_first_final_cordinate=the_first_final_cordinate,The_first_initial_cordinate
+                    the_first_initial_cordinate,the_first_final_cordinate=the_first_final_cordinate,the_first_initial_cordinate
                 if the_second_initial_cordinate>the_second_final_cordinate:
-                    the_second_initial_cordinate,the_second_final_cordinate=the_second_final_cordinate,The_second_initial_cordinate
+                    the_second_initial_cordinate,the_second_final_cordinate=the_second_final_cordinate,the_second_initial_cordinate
                 if the_first_initial_cordinate==the_first_final_cordinate:
                     for j in range(numerical_number_of_ship_decks):
-                        if the_second_final_cordinate-the_second_initial_cordinate+1==numerical_number_of_ship_decks and game_board[the_players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate + j] != 2 and game_board[the_players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate + j] != 1:
+                        if the_second_final_cordinate-the_second_initial_cordinate+1==numerical_number_of_ship_decks and game_board[players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate + j] != 2 and game_board[players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate + j] != 1:
                             u=1
                 elif the_second_initial_cordinate == the_second_final_cordinate:
                     for j in range(numerical_number_of_ship_decks):
-                        if the_first_final_cordinate-the_first_initial_cordinate+1==numerical_number_of_ship_decks and game_board[the_players_digital_number][the_first_initial_cordinate+j][the_second_initial_cordinate]!=2 and game_board[the_players_digital_number][the_first_initial_cordinate+j][the_second_initial_cordinate]!=1:
+                        if the_first_final_cordinate-the_first_initial_cordinate+1==numerical_number_of_ship_decks and game_board[players_digital_number][the_first_initial_cordinate+j][the_second_initial_cordinate]!=2 and game_board[players_digital_number][the_first_initial_cordinate+j][the_second_initial_cordinate]!=1:
                             u=1
                 if u==1:
                     break
@@ -142,11 +142,11 @@ for the_players_digital_number in range(2):
                     if the_second_initial_cordinate==0:
                         Perimeter_multideck(the_first_initial_cordinate-1,3,the_second_initial_cordinate,numerical_number_of_ship_decks+1)
                     elif the_second_final_cordinate==9:
-                        Perimeter_multideck(the_first_initial_cordinate-1,3,9-numerical_number_of_ship_decks,Numerical_number_of_ship_decks+1)
+                        Perimeter_multideck(the_first_initial_cordinate-1,3,9-numerical_number_of_ship_decks,numerical_number_of_ship_decks+1)
                     else:
                         Perimeter_multideck(the_first_initial_cordinate-1,3,the_second_initial_cordinate-1,numerical_number_of_ship_decks+2)
                 for i in range(numerical_number_of_ship_decks):
-                    game_board[the_players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate+i]=1
+                    game_board[players_digital_number][the_first_initial_cordinate][the_second_initial_cordinate+i]=1
             elif the_second_initial_cordinate==the_second_final_cordinate:
                 if the_second_initial_cordinate==0:
                     if the_first_initial_cordinate==0:
@@ -158,7 +158,7 @@ for the_players_digital_number in range(2):
                 elif the_second_initial_cordinate==9:
                     if the_first_initial_cordinate==0:
                         Perimeter_multideck(0,numerical_number_of_ship_decks+1,8,2)
-                    elif The_first_final_cordinate==9:
+                    elif the_first_final_cordinate==9:
                         Perimeter_multideck(9-numerical_number_of_ship_decks,numerical_number_of_ship_decks+1,8,2)
                     else:
                         Perimeter_multideck(the_first_initial_cordinate-1,numerical_number_of_ship_decks+2,8,2)
@@ -202,7 +202,7 @@ while victory_counter[0]!=0 or victory_counter[1]!=0:
                     Y=1
                 i=ord(d[0])-1040-Y
                 j=d[0]-1
-                if game_board[1-the_players_digital_number][i][j]==3:
+                if game_board[1-players_digital_number][i][j]==3:
                     break
             if game_board[1-players_digital_number][i][j]==1:
                 victory_counter[players_digital_number]-=1
@@ -227,9 +227,9 @@ while victory_counter[0]!=0 or victory_counter[1]!=0:
                     else:
                         print(game_board[players_digital_number][j][i],end=' ')
                 print()
-if first_players_victory_counter==0:
+if victory_counter[0]==0:
     print('Первый игрок победил!')
-if second_players_victory_counter==0:
+if victory_counter[1]==0:
     print('Второй игрок победил!')
 for players_digital_number in range(2):
     print('Поле'+Letter_number_of_the_player()+'ого игрока:')
